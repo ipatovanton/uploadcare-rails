2312997ad9a0892e62ce2a49df52975ba167d166
Anton <ipatovanton@gmail.com>
2024-07-31 16:58:16 +0300

    Update mount_uploadcare_file.rb

--- lib/uploadcare/rails/mongoid/mount_uploadcare_file.rb
+++ lib/uploadcare/rails/mongoid/mount_uploadcare_file.rb
@@ -1,7 +1,4 @@
-# frozen_string_literal: true
-
 require 'mongoid'
-require 'active_support/concern'
 require 'uploadcare/rails/services/id_extractor'
 require 'uploadcare/rails/jobs/delete_file_job'
 require 'uploadcare/rails/jobs/store_file_job'
@@ -9,68 +6,50 @@ require 'uploadcare/rails/jobs/store_file_job'
 module Uploadcare
   module Rails
     module Mongoid
-      # A module containing Mongoid extension. Allows to use uploadcare file methods in Rails models
+      # A module containing Mongoid extension. Allows to use uploadcare file methods in Mongoid models
       module MountUploadcareFile
         extend ActiveSupport::Concern
 
         def build_uploadcare_file(attribute)
-          cdn_url = send(attribute).to_s
+          cdn_url = self[attribute].to_s
           return if cdn_url.empty?
 
           uuid = IdExtractor.call(cdn_url)
-          cache_key = Uploadcare::Rails::File.build_cache_key(cdn_url)
+          cache_key = File.build_cache_key(cdn_url)
           default_attributes = { cdn_url: cdn_url, uuid: uuid.presence }
           file_attributes = ::Rails.cache.read(cache_key).presence || default_attributes
           Uploadcare::Rails::File.new(file_attributes)
         end
 
         class_methods do
+          # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
           def mount_uploadcare_file(attribute)
             define_method attribute do
-              build_uploadcare_file(attribute)
+              build_uploadcare_file attribute
             end
 
             define_method "uploadcare_store_#{attribute}!" do |store_job = StoreFileJob|
-              return if self.instance_variable_get(:@uploadcare_processing)
-
               file_uuid = public_send(attribute)&.uuid
               return unless file_uuid
+              return store_job.perform_later(file_uuid) if Uploadcare::Rails.configuration.store_files_async
 
-              self.instance_variable_set(:@uploadcare_processing, true)
-
-              if Uploadcare::Rails.configuration.store_files_async
-                store_job.perform_later(file_uuid)
-              else
-                Uploadcare::FileApi.store_file(file_uuid)
-              end
-
-              self.instance_variable_set(:@uploadcare_processing, false)
+              Uploadcare::FileApi.store_file(file_uuid)
             end
 
             define_method "uploadcare_delete_#{attribute}!" do |delete_job = DeleteFileJob|
               file_uuid = public_send(attribute)&.uuid
               return unless file_uuid
+              return delete_job.perform_later(file_uuid) if Uploadcare::Rails.configuration.delete_files_async
 
-              if Uploadcare::Rails.configuration.delete_files_async
-                delete_job.perform_later(file_uuid)
-              else
-                Uploadcare::FileApi.delete_file(file_uuid)
-              end
+              Uploadcare::FileApi.delete_file(file_uuid)
             end
 
-            set_callback(:save, :around) do |document, block|
-              if document.changed.include?(attribute.to_s)
-                document.public_send("uploadcare_store_#{attribute}!")
-              end
-              block.call
-            end
+            after_save "uploadcare_store_#{attribute}!".to_sym unless Uploadcare::Rails.configuration.do_not_store
+            return unless Uploadcare::Rails.configuration.delete_files_after_destroy
 
-            set_callback(:destroy, :before) do |document|
-              if Uploadcare::Rails.configuration.delete_files_after_destroy
-                document.public_send("uploadcare_delete_#{attribute}!")
-              end
-            end
+            after_destroy "uploadcare_delete_#{attribute}!".to_sym
           end
+          # rubocop:enable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
         end
       end
     end
